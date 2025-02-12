2d7c41f47d179d5e8a8b59a133edf29fa9a18694
sushant-ghimire <sushantghimire098@gmail.com>
2025-02-12 23:21:10 +0545

    first commit

---
+++ muntiply2.java
@@ -0,0 +1,7 @@
+public class muntiply2 {
+    public static void main(String[] args){
+        int num=100;
+        System.out.println("The number is "+ num);
+        System.out.println("The doubled number is "+ num*2);
+    }
+}
